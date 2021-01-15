25362e38b4d6cb9d5aecfb8beb880b81ff12e82e
Luciano Bestia <LucianoBestia@gmail.com>
2021-01-15 11:25:58 +0100

    readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
-//! rust_wasm_websocket
+// region: lmake_md_to_doc_comments include README.md A //!
+//! # rust_wasm_websocket
+//!
+//! **crate for wasm websocket**  
+//! ***[repo](https://github.com/LucianoBestia/rust_wasm_websocket); version: 0.5.2  date: 2021-01-15 authors: Luciano Bestia***  
+//!
+//! [![crates.io](https://meritbadge.herokuapp.com/qrcode53bytes)](https://crates.io/crates/qrcode53bytes)
+//! [![Documentation](https://docs.rs/qrcode53bytes/badge.svg)](https://docs.rs/qrcode53bytes/)
+//! [![crev reviews](https://web.crev.dev/rust-reviews/badge/crev_count/qrcode53bytes.svg)](https://web.crev.dev/rust-reviews/crate/qrcode53bytes/)
+//! [![RustActions](https://github.com/LucianoBestia/qrcode53bytes/workflows/rust/badge.svg)](https://github.com/LucianoBestia/qrcode53bytes/)
+//! [![latest doc](https://img.shields.io/badge/latest_docs-GitHub-orange.svg)](https://lucianobestia.github.io/qrcode53bytes/qrcode53bytes/index.html)
+//! [![Licence](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/LucianoBestia/qrcode53bytes/blob/master/LICENSE)
+//!
+//! [![Lines in Rust code](https://img.shields.io/badge/Lines_in_Rust-237-green.svg)](https://github.com/LucianoBestia/rust_wasm_websocket/)
+//! [![Lines in Doc comments](https://img.shields.io/badge/Lines_in_Doc_comments-21-blue.svg)](https://github.com/LucianoBestia/rust_wasm_websocket/)
+//! [![Lines in Comments](https://img.shields.io/badge/Lines_in_comments-50-purple.svg)](https://github.com/LucianoBestia/rust_wasm_websocket/)
+//! [![Lines in examples](https://img.shields.io/badge/Lines_in_examples-0-yellow.svg)](https://github.com/LucianoBestia/rust_wasm_websocket/)
+//! [![Lines in tests](https://img.shields.io/badge/Lines_in_tests-0-orange.svg)](https://github.com/LucianoBestia/rust_wasm_websocket/)
+//!
+//! ## websocket
+//!
+//! The library contains the generic parts.  
+//! The specific implementation needs to implement the WebSocketTrait.  
+//!
+//! ## cargo crev reviews and advisory
+//!
+//! It is recommended to always use [cargo-crev](https://github.com/crev-dev/cargo-crev)  
+//! to verify the trustworthiness of each of your dependencies.  
+//! Please, spread this info.  
+//! On the web use this url to read crate reviews. Example:  
+//! <https://web.crev.dev/rust-reviews/crate/num-traits/>  
+//!
+// endregion: lmake_md_to_doc_comments include README.md A //!
 
 pub mod websocket_mod;
